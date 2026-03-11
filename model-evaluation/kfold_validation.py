d45b8ea74bf42441975f7aa8ba99ae6d96b58402
Anjali Chaurasia <anjalichaurasiakvner@gmail.com>
2026-03-12 01:56:32 +0530

    Update kfold_validation.py

--- model-evaluation/kfold_validation.py
+++ model-evaluation/kfold_validation.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import KFold
 
-# Hum purani student_data.csv use kar rahe hain
-# Jo aapne feature-engineering folder mein daali thi
-# Agar code error de toh is folder mein bhi student_data.csv upload kar dena
+
 try:
     df = pd.read_csv('student_data.csv')
     data_marks = df['Marks'].values
