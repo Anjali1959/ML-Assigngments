bbf1bf098d5d9fa2976d6c5fee52dabfda7d9931
Anjali1959 <anjalichaurasiakvner@gmail.com>
2026-03-12 00:10:14 +0530

    Create tensors_practice.py

---
+++ numpy-basics/tensors_practice.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# 1. Scalar
+scalar = np.array(5)
+print("Scalar:", scalar)
+
+# 2. Vector
+vector = np.array([10, 20, 30, 40])
+print("Vector:", vector)
+
+# 3. 3D Tensor
+tensor_3d = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print("3D Tensor:\n", tensor_3d)
